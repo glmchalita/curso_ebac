f63602d91fe3ce3a9cd946a83022f5b952fa6b51
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-07-11 21:06:02 -0300

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-  console.log("teste");
-});
+  $("#celular").mask("(00)00000-0000");
+  $("#cpf").mask("000.000.000-00");
+  $("#cep").mask("00000-000");
 
-$("#form-tarefa").on("submit", function (e) {
-  e.preventDefault();
-
-  const nomeTarefa = $("#nome-tarefa").val();
-  $(`<li>${nomeTarefa}</li>`).appendTo("#lista-tarefa");
-
-  $("#nome-tarefa").val("");
-});
-
-$("#lista-tarefa").on("click", "li", function () {
-  $(this).toggleClass("linha");
+  $("form").validate({
+    rules: {
+      nome: { required: true },
+      email: { required: true, email: true },
+      celular: { required: true },
+      cpf: { required: true },
+      endereco: { required: true },
+      cep: { required: true },
+    },
+  });
 });
