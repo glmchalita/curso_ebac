b48968fb4957c123d4e3300718319fb8b419f8f9
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-10-18 14:28:34 -0300

    Add TS Exercise

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplication = (num1, num2) => num1 * num2;
+console.log(multiplication(5, 2));
+const hello = (name) => `Hello ${name}`;
+console.log(hello("Guilherme"));
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const multiplication = (num1: number, num2: number): number => num1 * num2;
+
+console.log(multiplication(5, 2));
+
+const hello = (name: string): string => `Hello ${name}`;
+
+console.log(hello("Guilherme"));
