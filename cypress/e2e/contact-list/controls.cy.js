d019e2941a8718273985982822d5f7902ed9773c
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-02-14 16:38:50 -0300

    reactor: write a solid test

--- cypress/e2e/contact-list/controls.cy.js
+++ cypress/e2e/contact-list/controls.cy.js
@@ -8,23 +8,26 @@ describe('Testes para controles dos contatos', () => {
   })
   
   it('Deve adicionar um novo contato', () => {
-    cy.get('@contacts').should('have.length', 3)
-    cy.get('input[placeholder*="Nome"]').type('Guilherme')
-    cy.get('input[placeholder*="E-mail"]').type('guilherme@gmail.com')
-    cy.get('input[placeholder*="Telefone"]').type('11912341234{enter}')
-    cy.get('@contacts').should('have.length', 4)
+    cy.get('@contacts', {timeout: 10000}).then(contatos => {
+      let length = contatos.length
+      cy.get('input[placeholder*="Nome"]').type('Guilherme')
+      cy.get('input[placeholder*="E-mail"]').type('guilherme@gmail.com')
+      cy.get('input[placeholder*="Telefone"]').type('11912341234{enter}')
+      cy.get('@contacts').should('have.length', length + 1)
+    })
   })
 
   it('Deve remover um contato', () => {
-    cy.get('@contacts').should('have.length', 4)
-    cy.contains('.contato', 'Guilherme').find('.delete').click();
-    cy.get('@contacts').should('have.length', 3)
+    cy.get('@contacts', {timeout: 10000}).then(contatos => {
+      let length = contatos.length
+      cy.get('@contacts').first().find('.delete').click();
+      cy.get('@contacts').should('have.length', length - 1)
+    })
   })
 
   it('Deve editar um contato', () => {
-    cy.get('@contacts').should('have.length', 3)
-    cy.contains('.contato', 'ian').find('.edit').click();
-    cy.get('input[placeholder*="Nome"]').clear().type('Gian Souza{enter}')
-    cy.get('li').should('contain', 'Gian Souza')
+    cy.get('@contacts').first().find('.edit').click();
+    cy.get('input[placeholder*="Nome"]').clear().type('Banana Split{enter}');
+    cy.contains('.contato', 'Banana Split').should('exist');
   })
 })
